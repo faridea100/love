5bc057c7b64378a8201edf9cfd47c8f54f34a9c7
faridea100 <far.idea100@gmail.com>
2025-07-09 16:59:43 +0530

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -27,21 +27,51 @@ export default defineConfig({
         scope: '/',
         orientation: 'portrait',
         icons: [
+         
           {
+            src: '/assets/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: "image/png",
+          },
+          {
+            src: '/assets/icons/icon-512x512.jpg',
+            sizes: '512x512',
+            type: "image/jpeg",
+          },
+          {
+            src: '/assets/icons/icon-512x512.svg',
+            sizes: '512x512',
+            type: "image/svg",
+          },
+           {
             src: '/assets/icons/icon-192x192.png',
             sizes: '192x192',
+            type: "image/png",
+          },
+          {
+            src: '/assets/icons/icon-192x192.jpg',
+             sizes: '192x192',
+            type: "image/jpeg",
+          },
+           {
+            src: '/assets/icons/icon-192x192.svg',
+             sizes: '192x192',
+            type: "image/svg",
+          },
+          {
+            src: '/assets/icons/icon-144x144.png',
+            sizes: '144x144',
+            type: "image/png",
+          },
+          {
+            src: '/assets/icons/icon-144x144.jpg',
+             sizes: '144x144',
             type: "image/jpeg",
           },
           {
-            src: '/assets/icons/icon-512x512.jpg',
-            sizes: '512x512',
-            type: "image/jpeg",
-          },
-          {
-            src: '/assets/icons/icon-512x512.jpg',
-            sizes: '512x512',
-            type: "image/jpeg",
-            purpose: 'any maskable',
+            src: '/assets/icons/icon-144x144.svg',
+            sizes: '144x144',
+            type: "image/svg",
           },
         ],
         screenshots: [
@@ -55,6 +85,18 @@ export default defineConfig({
             src: "/assets/screenshots/mobile-view.jpg",
             sizes: "600x849",
             type: "image/jpeg",
+            form_factor: "narrow"
+          },
+          {
+            src: "/assets/screenshots/home.png",
+            sizes: "600x849",
+            type: "image/png",
+            form_factor: "wide",
+          },
+          {
+            src: "/assets/screenshots/mobile-view.png",
+            sizes: "600x849",
+            type: "image/png",
             form_factor: "narrow"
           }
         ]
