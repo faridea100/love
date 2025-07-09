920b5b0b3fd853563cf6941752a71f8c2e382dd5
faridea100 <far.idea100@gmail.com>
2025-07-09 16:35:07 +0530

    updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
-import { InstallButton } from './install-button';
-
 export const routes: Routes = [
-  { path: '', component: InstallButton, },
-  { path: 'home', loadComponent: () => import('./home').then((t) => t.Home) , pathMatch:'full'},
+  { path: 'home', loadComponent: () => import('./home').then((t) => t.Home) },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'img', loadComponent: () => import('./imgs').then((t) => t.Imgs) },
-  { path: 'img/:id', loadComponent: () => import('./imgs').then((t) => t.Imgs) },
-  { path: '**', redirectTo:'home'}
+  {
+    path: 'img/:id',
+    loadComponent: () => import('./imgs').then((t) => t.Imgs),
+  },
+  { path: '**', redirectTo: 'home' },
 ];
